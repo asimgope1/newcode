3608d116c73a19aed77802a227647d5be1f8aa3e
asimgope <asimgope53@gmail.com>
2023-11-05 08:27:49 +0530

    testing1

--- src/constants/color.js
+++ src/constants/color.js
@@ -4,3 +4,6 @@ export const PINK = '#f095b1';
 export const WHITE = 'white';
 export const BLACK = 'black';
 export const BLUE = '#3056ff';
+export const DARKBLUE = '#046c88';
+export const SKIN = '#f5ede0';
+
