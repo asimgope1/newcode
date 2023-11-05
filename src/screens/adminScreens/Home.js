d434eae19c276b58be4197f104c2412371642597
asimgope <asimgope53@gmail.com>
2023-11-05 08:36:27 +0530

    testing3.1

--- src/screens/adminScreens/Home.js
+++ src/screens/adminScreens/Home.js
@@ -1,7 +1,49 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image, ImageBackground, View } from 'react-native';
+import { MyStatusBar } from '../../constants/config';
+import { ORANGE } from '../../constants/color';
+import { BACKGROUND, LOGO } from '../../constants/imagepath';
 export default Home = () => {
     return (
-        <View style={{ flex: 1, }}></View>
+        <View style={{ flex: 1,
+        
+        }}>
+          
+            <ImageBackground
+                style={{
+                    flex: 1,
+                    // justifyContent: 'center',
+                    alignItems: 'center'
+
+                }}
+                source={BACKGROUND}
+            
+            >
+                 <Image
+                resizeMode="contain"
+                    style={{
+                     
+                    
+                        height: '60%',
+                        width: '60%'
+                    }}
+                    source={LOGO}
+                />
+                <View
+                style={{
+                    height:'50%',
+                    width:'90%',
+                //   
+                    // backgroundColor:ORANGE
+                }}
+                >
+
+                </View>
+
+
+            </ImageBackground>
+
+
+        </View>
     )
 }
